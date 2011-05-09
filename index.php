79b05e7c41d7e43f3d8651d880b782e6d1c8ad92
Markus Bürgler <markus.buergler@me.com>
2011-05-09 17:01:24 +0200

    jQuery Mobile added

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title> MCI Rooms&Lectures</title> 
+		<link rel="apple-touch-icon" href="img/tutsTouchIcon.png" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
+		<link rel="stylesheet" href="styles/screen.css" />
+		<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+</head>
+<body>
+	<div data-role="page" data-fullscreen="true">
+		<header data-role="header" data-position="fixed">
+			<h1>MCI</h1>
+		</header>
+		<div data-role="content">
+			
+		</div>
+		<footer data-role="footer" data-position="fixed">
+			<div data-role="navbar">
+				<ul>
+					<li><a data-icon="custom" id="btnLectures" href="#">Lectures</a></li>
+					<li><a data-icon="custom" id="btnSpeakers" href="#">Speakers</a></li>
+					<li><a data-icon="custom" id="btnLocations" href="#">Locations</a></li>
+				</ul>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
